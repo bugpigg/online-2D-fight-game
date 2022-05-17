a0df637c7bef8517e17b996309efc1aec3e5c0d1
bugpigg <bugpigg@gmail.com>
2022-05-17 16:29:09 +0900

    feat: Member 도메인 클래스 생성

---
+++ src/main/java/com/bugpigg/game/auth/domain/Member.java
@@ -0,0 +1,45 @@
+package com.bugpigg.game.auth.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String userName, String password, String email, Role role) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role.getKey();
+    }
+}
