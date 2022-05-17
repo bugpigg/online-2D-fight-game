cd6c47de56d29d4e87a072f722b5014135a7cf4c
bugpigg <bugpigg@gmail.com>
2022-05-17 16:43:06 +0900

    feat: http 요청의 헤더부분을 파싱하는 클래스 추가

---
+++ src/main/java/com/bugpigg/game/auth/util/HeaderParser.java
@@ -0,0 +1,28 @@
+package com.bugpigg.game.auth.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HeaderParser {
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    public static String getAccessRequestToken(HttpServletRequest request) {
+        String headerValue = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (headerValue == null) {
+            return null;
+        }
+
+        if (headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
+    }
+
+    public static void setAccessTokenInHeader(HttpServletResponse response,
+        String accessToken) {
+        response.setHeader(HEADER_AUTHORIZATION, TOKEN_PREFIX + accessToken);
+    }
+}
